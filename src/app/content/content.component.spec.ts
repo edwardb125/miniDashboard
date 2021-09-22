e8e6ebb6771bca97c6332e11b5ba8a5853b00e6e
erfan hemati <erfanhematig@gmail.com>
2021-09-23 01:27:52 +0330

    init content.spec

--- src/app/content/content.component.spec.ts
+++ src/app/content/content.component.spec.ts
@@ -1,25 +1,43 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { ContentComponent } from './content.component';
+import { ContentComponent } from './content.component';
+import {ContentService} from "./content.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {AngularFirestore} from "@angular/fire/compat/firestore";
 
-// describe('ContentComponent', () => {
-//   let component: ContentComponent;
-//   let fixture: ComponentFixture<ContentComponent>;
+describe('ContentComponent', () => {
+  let component: ContentComponent;
+  let fixture: ComponentFixture<ContentComponent>;
+  let contentService: ContentService;
+  let snackBar: MatSnackBar;
+  let afs: AngularFirestore;
+  let store: AngularFirestore;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ ContentComponent ]
-//     })
-//     .compileComponents();
-//   });
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ContentComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(async () => {
+    const SpyService = jasmine.createSpyObj('ContentService',[])
+    const SpySnack = jasmine.createSpyObj('MatSnackBar',[])
+    const SpyAfs = jasmine.createSpyObj('AngularFirestore',[])
+    const SpyStore = jasmine.createSpyObj('AngularFirestore',[])
+    await TestBed.configureTestingModule({
+      declarations: [ ContentComponent ],
+      providers: [
+        {provide: ContentService, useValue: SpyService},
+        {provide: MatSnackBar, useValue: SpySnack},
+        {provide: AngularFirestore, useValue: SpyAfs},
+        {provide: AngularFirestore, useValue: SpyStore}
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+      ]})
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ContentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
